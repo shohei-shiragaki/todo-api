7d23be2303ac912595b03fdbb74048192eddf59f
shohei-shiragaki <126951181+shohei-shiragaki@users.noreply.github.com>
2025-01-16 23:32:39 +0900

    デプロイ失敗修正10

---
+++ db/versions/3bd8ff9f29a5_createtable.py
@@ -0,0 +1,23 @@
+"""createTable
+
+Revision ID: 3bd8ff9f29a5
+Revises: 
+Create Date: 2025-01-14 22:11:19.922104
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = 'aec8b9a3321a'
+down_revision: Union[str, None] = None
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
